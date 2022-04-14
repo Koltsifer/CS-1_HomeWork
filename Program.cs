9af90fac8b28184cab69ad9626e8e9af6372a939
Dmitriy <koltsifer@gmail.com>
2022-04-14 16:52:35 +0300

    Main created

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.WriteLine("Укажите номер задачи");
+                key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        Task1();
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        Task2();
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        Task3();
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.D4:
+                        Console.Clear();
+                        Task4();
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.D5:
+                        Console.Clear();
+                        Task5();
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Escape);
+        }
+        static void Task1()
+        {
+
+        }
+
+        static void Task2()
+        {
+
+        }
+
+        static void Task3()
+        {
+
+        }
+
+        static void Task4()
+        {
+
+        }
+
+        static void Task5()
+        {
+
+        }
+    }
+}
