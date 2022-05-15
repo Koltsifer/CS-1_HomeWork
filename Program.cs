19cc6fcf7e1f32d2ed2eedd5e9f8b3652e376479
Dmitriy <koltsifer@gmail.com>
2022-05-15 23:17:44 +0300

    Task2 complete

--- Program.cs
+++ Program.cs
@@ -6,13 +6,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+/*
+
+    1. Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 символов, 
+содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+а) без использования регулярных выражений;
+б) **с использованием регулярных выражений.
+
+    2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+а) Вывести только те слова сообщения, которые содержат не более n букв.
+б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+в) Найти самое длинное слово сообщения.
+г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+д) ***Создать метод, который производит частотный анализ текста. В качестве параметра в него передается массив слов и текст, 
+в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст. Здесь требуется использовать класс Dictionary.
+
+    3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+Например: badc являются перестановкой abcd.
+
+    4. *Задача ЕГЭ.
+На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов некоторой средней школы. В первой строке сообщается количество учеников N, 
+которое не меньше 10, но не превосходит 100, каждая из следующих N строк имеет следующий формат:
+
+<Фамилия> <Имя> <оценки>,
+где <Фамилия> — строка, состоящая не более чем из 20 символов, <Имя> — строка, состоящая не более чем из 15 символов, <оценки> — через пробел три целых числа, 
+соответствующие оценкам по пятибалльной системе. <Фамилия> и <Имя>, а также <Имя> и <оценки> разделены одним пробелом. Пример входной строки:
+
+Иванов Петр 4 5 3
+Требуется написать как можно более эффективную программу, которая будет выводить на экран фамилии и имена трёх худших по среднему баллу учеников. 
+Если среди остальных есть ученики, набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
+
+Достаточно решить 2 задачи. Старайтесь разбивать программы на подпрограммы. Переписывайте в начало программы условие и свою фамилию. 
+Все программы сделать в одном решении. Для решения задач используйте неизменяемые строки (string).
+
+*/
+
 namespace HomeWork5
 {
     internal class Program
     {
+        readonly static string message = "RegExr was created by gskinner, and is proudly hosted by Media TempleEdit the Expression and Text to see matches";
+
         static class Message
         {
-            public static void StringOutMax(string message, int count)
+            public static void WordOutMax(string message, int count)
             {
                 string result = string.Empty;
                 string pattern = @"\b\w{" + count + @"}\b";
@@ -21,7 +58,7 @@ namespace HomeWork5
                 Console.WriteLine(result);
             }
 
-            public static void StringDeleteByEnd(string message, char input)
+            public static void WordDeleteByEndChar(string message, char input)
             {
                 string result = "";
                 string pattern = "\\w+[^" + input + " ]\\b";
@@ -31,17 +68,47 @@ namespace HomeWork5
                 
             }
 
-            public static void LongestString(string message)
+            public static void LongestWord(string message)
             {
-                string result = "";
-                foreach (Match m in Regex.Matches(message, @"\w+"))
-                    if (m.Length > result.Length)
+                StringBuilder result = new StringBuilder("");
+                foreach (Match m in Regex.Matches(message, @"\b[a-zA-Z]+\b"))
+                    if (m.Length > result.Length || m.Length == result.Length)
                     {
-                        result = m.ToString();
                         if (m.Length == result.Length)
-                            result = result + " " + m.ToString();
+                            result.Append(" " + m);
+                        else
+                        {
+                            result.Clear();
+                            result.Append(m);
+                        }
                     }
                 Console.WriteLine(result);
+            }
+
+            public static void AnalyseRepeatingWords(string message)
+            {
+                Console.WriteLine("WordAnalyser\n");
+
+                Dictionary<string, int> words = new Dictionary<string, int>()
+                {
+                    ["hello"] = 0,
+                    ["by"] = 0,
+                    ["media"] = 0,
+                    ["created"] = 0,
+                    ["tree"] = 0,
+                    ["match"] = 0,
+                    ["and"] = 0
+                };
+
+                foreach (Match m in Regex.Matches(message.ToLower(), @"\b[a-zA-Z]+\b"))
+                    if (words.ContainsKey(m.Value))
+                        words[m.ToString()]++;
+
+                foreach (var pair in words)
+                {
+                    if (pair.Value != 0)
+                        Console.WriteLine(pair.Key + " " + pair.Value);
+                }
             }
         }
 
@@ -72,9 +139,7 @@ namespace HomeWork5
             return flag;
         }
 
-        readonly static string message = "RegExr was created by gskinner, and is proudly hosted by Media TempleEdit the Expression and Text to see matches";
-
-        static void Main(string[] args)
+        static void Main()
         {
             ConsoleKeyInfo key;
             do
@@ -112,13 +177,6 @@ namespace HomeWork5
 
         static void Task1()
         {
-            /*
-            1.Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 символов, 
-            содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
-            а) без использования регулярных выражений;
-            б) **с использованием регулярных выражений.
-            */
-
             //Console.Write("Введите логин: ");
             //string a = "1cudjh23j";
             //a = Console.ReadLine();
@@ -147,31 +205,16 @@ namespace HomeWork5
 
         static void Task2()
         {
-            /*
-            2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
-            а) Вывести только те слова сообщения, которые содержат не более n букв.
-            б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
-            в) Найти самое длинное слово сообщения.
-            г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
-            д) ***Создать метод, который производит частотный анализ текста. В качестве параметра в него передается массив слов и текст, 
-            в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст. Здесь требуется использовать класс Dictionary.
-            */
-
-            Message.StringOutMax(message, 3);
-            //Message.StringDeleteByEnd(message, 'y');
-            Message.LongestString(message);
+            Message.WordOutMax(message, 3);
+            Message.WordDeleteByEndChar(message, 'r');
+            Message.LongestWord(message);
             Console.WriteLine();
+            Message.AnalyseRepeatingWords(message);
             Console.ReadKey();
-
         }
 
         static void Task3()
         {
-            /*
-            3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
-            Например: badc являются перестановкой abcd.
-            */
-
             //string a = "fk";
             //string b = "ap";
             //int x = 0, y = 0;
@@ -201,27 +244,9 @@ namespace HomeWork5
 
         static void Task4()
         {
-            /*
-            4. *Задача ЕГЭ.
-            На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов некоторой средней школы. В первой строке сообщается количество учеников N, 
-            которое не меньше 10, но не превосходит 100, каждая из следующих N строк имеет следующий формат:
-
-            <Фамилия> <Имя> <оценки>,
-            где <Фамилия> — строка, состоящая не более чем из 20 символов, <Имя> — строка, состоящая не более чем из 15 символов, <оценки> — через пробел три целых числа, 
-            соответствующие оценкам по пятибалльной системе. <Фамилия> и <Имя>, а также <Имя> и <оценки> разделены одним пробелом. Пример входной строки:
-
-            Иванов Петр 4 5 3
-            Требуется написать как можно более эффективную программу, которая будет выводить на экран фамилии и имена трёх худших по среднему баллу учеников. Если среди остальных есть ученики, 
-            набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
-
-            Достаточно решить 2 задачи. Старайтесь разбивать программы на подпрограммы. Переписывайте в начало программы условие и свою фамилию. Все программы сделать в одном решении. 
-            Для решения задач используйте неизменяемые строки (string).
-            */
-
             StreamReader sr = new StreamReader("data.txt");
             sr.Close();
             Console.WriteLine("Portable Test!");
         }
-
     }
 }
